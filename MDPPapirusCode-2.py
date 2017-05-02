1e3b909f136749a40722d37f195196b39a643c94
Priyanka Agarwal <pragarwal.agarwal@gmail.com>
2017-05-02 11:57:27 -0700

    Fixed errors

--- MDPPapirusCode-2.py
+++ MDPPapirusCode-2.py
@@ -77,8 +77,6 @@ def main():
     # Use smaller font for smaller dislays
     if papirus.height <= 96:
         SIZE = 18
-    papirus.clear()
-    write_text(papirus, "Ready... SW1 + SW2 to exit.", SIZE)
     while True:
         if GPIO.input(SW2) == False:
             write_text(papirus, "Two", SIZE)
@@ -86,40 +84,7 @@ def main():
             write_text(papirus, "Three", SIZE)
         sleep(0.1)
 
-def write_text(papirus, text, size):
 
-    # initially set all white background
-    image = Image.new('1', papirus.size, WHITE)
-
-    # prepare for drawing
-    draw = ImageDraw.Draw(image)
-
-    font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf', size)
-
-    # Calculate the max number of char to fit on line
-    line_size = (papirus.width / (size*0.65))
-
-    current_line = 0
-    text_lines = [""]
-
-    # Compute each line
-    for word in text.split():
-        # If there is space on line add the word to it
-        if (len(text_lines[current_line]) + len(word)) < line_size:
-            text_lines[current_line] += " " + word
-        else:
-            # No space left on line so move to next one
-            text_lines.append("")
-            current_line += 1
-            text_lines[current_line] += " " + word
-
-    current_line = 0
-    for l in text_lines:
-        current_line += 1
-        draw.text( (0, ((size*current_line)-size)) , l, font=font, fill=BLACK)
-
-    papirus.display(image)
-    papirus.partial_update()
 
 if __name__ == '__main__':
     main()
